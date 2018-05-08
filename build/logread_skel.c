3bc84e353e4aea005d3f4e0c7c5e4c2584e5dcef
Justin Chan <justinhchann@gmail.com>
2018-05-08 16:53:27 -0400

    semi working logread -S, need to account for departures

--- build/logread_skel.c
+++ build/logread_skel.c
@@ -13,27 +13,51 @@
 #include <openssl/err.h>
 #include <openssl/rand.h>
 
+#define int_max 1073741823
+
 
 // state_or_rooms = 0 for current state
 // state_or_rooms = 1 for all rooms
 char* concat(const char *s1, const char *s2);
+int contains(const char *str, char **list, const int num);
+static int myCompare(const void * str1, const void * str2);
+void sort(char *list[], int num);
+
+struct room {
+  int room_num;
+  char **people;
+  int num_people;
+};
 
 int main(int argc, char *argv[]) {
-  const int int_max = 1073741823;
 
   FILE *fp;
   size_t line_length = 0;
   ssize_t len;
-  int i, opt, state_or_rooms = 0, is_employee = 0, is_guest = 0;
+  int i, j, exists, opt, state_or_rooms, is_employee = 0, is_guest = 0;
   char  *logpath = NULL,
     *TOKEN = NULL,
     *NAME = NULL,
     *line;
   extern char *optarg;
 
+  // decryption variables
+  // unsigned char *ciphertext, *plaintext, key[16], iv[16];
+  // int i_len, o_len1 = 0, o_len2 = 0;
+  // EVP_CIPHER_CTX ctx;
+
+
   // parsing variables
   char *i_temp, *i_name, *i_role, *prev_action, *i_action;
   int prev_time = 0, i_time, i_room;
+
+  // current state variables
+  char **employee_names = NULL, **guest_names = NULL;
+  int e_name_count = 0, g_name_count = 0;
+
+  int num_rooms;
+  struct room **all_rooms = NULL;
+
 
   // all rooms variables
   char *room_list = NULL, i_room_str[11];
@@ -45,9 +69,17 @@ int main(int argc, char *argv[]) {
     // printf("%c\n", opt);
     switch(opt) {
       case 'K':
-        // TODO: get the token
-        // check token is alphanumeric
-        // check if decrypt works
+        TOKEN = optarg;
+        // TODO: check token is alphanumeric
+        // TODO: check if decrypt works
+
+        // create key
+
+        // create iv
+        // int i = 0;
+        // for (i = 0; i < sizeof(iv); i++) {
+        //   iv[i] = '\x00';
+        // }
 
         break;
 
@@ -87,6 +119,8 @@ int main(int argc, char *argv[]) {
     logpath = argv[optind];
   }
   // printf("logpath: %s\n", logpath);
+  // printf("token: %s\n", TOKEN);
+  // printf("name: %s\n", NAME);
 
   // check for -R used with employee or guest
   if (state_or_rooms == 1 && (is_employee == 0 && is_guest == 0)) {
@@ -94,14 +128,46 @@ int main(int argc, char *argv[]) {
     return 255;
   }
 
-  // TODO: decrypt log file
-
   // get and check the log file
   fp = fopen(logpath, "r");
   if (fp == NULL) {
     printf("invalid\n");
     return 255;
   }
+
+  // // initial check for file not even containing tag
+  // if (f_size < 16)
+  //   return throw_inv();
+
+  // // detach tag
+  // i_len = f_size - 17;
+  // j = 0;
+  // for (i = 0; i < f_size; i++) {
+  //   if (i < (i_len)) {
+  //     // just the msg
+  //     i_msg[i] = buffer[i];
+  //   } else {
+  //     // the tag
+  //     tag[j] = buffer[i];
+  //     j++;
+  //   }
+  // }
+
+  // // Decrypt log file
+  // ctx = EVP_CIPHER_CTX_new();
+  // if (!EVP_DecryptInit(&ctx, EVP_aes_256_gcm(), key, iv)) {
+  //   printf("integrity violation\n");
+  //   return 255;
+  // }
+  // EVP_DecryptUpdate(&ctx, plaintext, &o_len1, ciphertext, i_len);
+
+  // // check tag
+  // if (!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, 16, tag)) {
+  //   printf("integrity violation\n");
+  //   return 255;        
+  // }
+  // EVP_DecryptFinal(&ctx, plaintext + o_len1, &o_len2);
+  // plaintext[o_len1+o_len2] = '\0';
 
   // go through the each line of the file
   while ((len = getline(&line, &line_length, fp)) != -1) {
@@ -169,10 +235,114 @@ int main(int argc, char *argv[]) {
       i_temp = strtok(NULL, "|");
       i++;
     }
-    i = 0;
 
     if (state_or_rooms == 0) {
       // TODO: current state line processing
+
+      if (strcmp(i_role, "EM") == 0) {
+        // get the names of employees
+
+        if (e_name_count == 0) {
+          // first name, add it
+          employee_names = (char **) realloc(employee_names, (e_name_count + 1) * sizeof(char *));
+          employee_names[e_name_count++] = strdup(i_name);
+
+        } else {
+          // check if name is already in
+          if (!contains(i_name, employee_names, e_name_count)) {
+            // add new name
+            employee_names = (char **) realloc(employee_names, (e_name_count + 1) * sizeof(char *));
+            employee_names[e_name_count++] = strdup(i_name);
+          }
+
+        }
+      } else if (strcmp(i_role, "GU") == 0) {
+        // get the names of guests
+
+        if (g_name_count == 0) {
+          // first name, add it
+          guest_names = (char **) realloc(guest_names, (g_name_count + 1) * sizeof(char *));
+          guest_names[g_name_count++] = strdup(i_name);
+
+        } else {
+          // check if name is already in
+          if (!contains(i_name, guest_names, g_name_count)) {
+            // add new name
+            guest_names = (char **) realloc(guest_names, (g_name_count + 1) * sizeof(char *));
+            guest_names[g_name_count++] = strdup(i_name);
+          }
+
+        }
+      }
+
+      // TODO: get room information
+      if (i_room != -1 && (strcmp(i_action, "AV") == 0)) {
+        // arriving in a room - add person to the room's list
+        if (num_rooms == 0) {
+          // first room - initialize array of room structs
+          all_rooms = (struct room **) realloc(all_rooms, (num_rooms + 1) * sizeof(struct room *));
+          all_rooms[num_rooms] = (struct room *) malloc(sizeof(struct room));
+
+          // add room info
+          all_rooms[num_rooms]->room_num = i_room;
+          all_rooms[num_rooms]->num_people = 0;
+          // add first person
+          all_rooms[num_rooms]->people = NULL;
+          all_rooms[num_rooms]->people = (char **) realloc(all_rooms[num_rooms]->people, (all_rooms[num_rooms]->num_people + 1) * sizeof(char *));
+          all_rooms[num_rooms]->people[all_rooms[num_rooms]->num_people++] = strdup(i_name);
+
+          num_rooms++;
+
+        } else {
+          // other occurances for rooms
+
+          // check if room already exists
+          exists = 0;
+          for (i = 0; i < num_rooms; i++) {
+            if (all_rooms[i]->room_num == i_room) {
+              exists = 1;
+              break;
+            }
+          }
+
+          if (exists == 1) {
+            // room exists, add person if not inside 
+            // printf("%d exists\n", i_room);
+
+            // check if person is already in the room's list
+            if (!contains(i_name, all_rooms[i]->people, all_rooms[i]->num_people)) {
+              // add new name
+              all_rooms[i]->people = (char **) realloc(all_rooms[i]->people, (all_rooms[i]->num_people + 1) * sizeof(char *));
+              all_rooms[i]->people[all_rooms[i]->num_people++] = strdup(i_name);
+            }
+
+          } else {
+            // printf("creating new room for %d\n", i_room);
+            // create new room
+            all_rooms = (struct room **) realloc(all_rooms, (num_rooms + 1) * sizeof(struct room *));
+            all_rooms[num_rooms] = (struct room *) malloc(sizeof(struct room));
+
+            // add room info
+            all_rooms[num_rooms]->room_num = i_room;
+            all_rooms[num_rooms]->num_people = 0;
+
+            // printf("adding %s\n", i_name);
+            // add new people
+            all_rooms[num_rooms]->people = NULL;
+            all_rooms[num_rooms]->people = (char **) realloc(all_rooms[num_rooms]->people, (all_rooms[num_rooms]->num_people + 1) * sizeof(char *));
+            all_rooms[num_rooms]->people[all_rooms[num_rooms]->num_people++] = strdup(i_name);
+            // printf("added %s\n", i_name);
+
+            num_rooms++;
+            // printf("created new room for %d\n", i_room);
+          }
+        }
+
+
+      } else if (i_room != -1 && (strcmp(i_action, "DP"))) {
+        // leaving a room - remove person from the room's list
+
+      }
 
 
     } else if (state_or_rooms == 1) {
@@ -216,18 +386,97 @@ int main(int argc, char *argv[]) {
     }
 
   }
+  fclose(fp);
 
     if (state_or_rooms == 0) {
       // TODO: print out current state
 
-    } else if (state_or_rooms == 1) {
+      // sort the employee and guest names
+      sort(employee_names, e_name_count);
+      sort(guest_names, g_name_count);
+
+      // print out employee names
+      for (i = 0; i < e_name_count; i++) {
+        if (i != 0) {
+          printf(",");
+        }
+        printf("%s", employee_names[i]);
+      }
+      printf("\n");
+
+      // print out guest names
+      for (i = 0; i < g_name_count; i++) {
+        if (i != 0) {
+          printf(",");
+        }
+        printf("%s", guest_names[i]);
+      }
+      printf("\n");
+
+      // free employee names
+      if (employee_names != NULL) {
+        for (i = 0; i < e_name_count; i++) {
+          free(employee_names[i]);
+        }
+        free(employee_names);
+      }
+
+      // free guest names
+      if (guest_names != NULL) {
+        for (i = 0; i < g_name_count; i++) {
+          free(guest_names[i]);
+        }
+        free(guest_names);
+      }
+
       // print out list of rooms
+      for(i = 0; i < num_rooms; i++) {
+        if (all_rooms[i]->num_people > 0) {
+          printf("%d: ", all_rooms[i]->room_num);
+
+          // sort the list of people
+          sort(all_rooms[i]->people, all_rooms[i]->num_people);
+
+          // print out room's people
+          for (j = 0; j < all_rooms[i]->num_people; j++) {
+            if (j != 0) {
+              printf(",");
+            }
+            printf("%s", all_rooms[i]->people[j]);
+          }
+          printf("\n");
+        }
+      }
+
+
+      // free list of rooms and its people
+      if (all_rooms != NULL) {
+        for (i = 0; i < num_rooms; i++) {
+          
+          // loop through
+          if (all_rooms[i]->people != NULL) {
+            for (j = 0; j < all_rooms[i]->num_people; j++) {
+              free(all_rooms[i]->people[j]);
+            }
+            free(all_rooms[i]->people);
+          }
+
+          free(all_rooms[i]);
+          // i++;
+        }
+        free(all_rooms);
+      }
+
+
+    } else if (state_or_rooms == 1) {
+      // print out list of rooms for person
       if (room_list != NULL) {
         printf("%s\n", room_list);
         free(room_list);
       }
     }
 
+    return 0;
 }
 
 
@@ -239,4 +488,21 @@ char* concat(const char *s1, const char *s2) {
   memcpy(result, s1, len1);
   memcpy(result + len1, s2, len2 + 1);
   return result;
+}
+
+int contains(const char *str, char **list, const int num) {
+  int i;
+  for (i = 0; i < num; i++) {
+    if (strcmp(list[i], str) == 0)
+      return 1;
+  }
+  return 0;
+}
+
+static int myCompare(const void * str1, const void * str2) {
+  return strcmp(*(const char **) str1, *(const char **) str2);
+}
+
+void sort(char *list[], int num) {
+  qsort(list, num, sizeof(const char *), myCompare);
 }
